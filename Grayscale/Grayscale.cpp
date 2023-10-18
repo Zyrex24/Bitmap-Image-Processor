f662a21b94c35a494c9fda6e8dc1eec132a44204
DIEMOS192 <omertarek131@gmail.com>
2023-10-18 18:19:35 +0300

    Assignment V4.5.4 Pre Release

--- Grayscale/Grayscale.cpp
+++ Grayscale/Grayscale.cpp
@@ -1,10 +1,9 @@
 // FCAI – OOP Programming – 2023 - Assignment 1
-// Program Name:				CS213-2023-20220452-20220438-A1-Part1.cpp
-// Last Modification Date:	7/10/2023
+// Program Name:				CS213-2023-20220452-20220438-A1-Full.cpp
+// Last Modification Date:	18/10/2023
 // Author1 and ID and Group:	Ahmed Niazi 20220452  S17
 // Author2 and ID and Group:	Omar Tarek 20220438  S17
-// Author3 and ID and Group:
-// Teaching Assistant:
+// Teaching Assistant:    Section 17
 // Purpose: Apply a variety of filters on grayscale bmp images
 
 
